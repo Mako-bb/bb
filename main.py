1ee90600ae53edaa96e55a7d837986fa4bc6dc96
Nicolas Viniegra <nicolasviniegra@gmail.com>
2020-11-20 15:06:08 -0300

    add amazon login example

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from platforms.acorntv_test_diego   import AcornTV_Test_Diego
 from platforms.acorntv_test_carlos  import AcornTV_Test_Carlos
 from platforms.optimum_test_diego   import Optimum_test_diego
 from platforms.optimum_test_carlos  import Optimum_Test_Carlos
+from platforms.amazon_login         import AmazonLogin
 from common                         import config
 
 logging.basicConfig(level=logging.INFO)
