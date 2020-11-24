a24a8c7067d9df5d43f2c4d09d986c9ca4333174
Carlos Mario Mosca <cmm@bb.vision>
2020-11-24 12:49:03 -0300

    Scraper completo, scrapea el json de cada episodio manualmente

--- platforms/cwtv_test_carlos.py
+++ platforms/cwtv_test_carlos.py
@@ -34,9 +34,10 @@ class CWtv_Test_Carlos():
 
     def _scraping(self, testing=False):
 
-        listPayload, listPayloadEpi = [], []
-        listDBEpi = []
-        listDB = []
+        listPayload = []
+        listPayloadEpi = []
+        listDBMovie = Datamanager._getListDB(self,self.titanScraping)
+        listDBEpi = Datamanager._getListDB(self,self.titanScrapingEpisodios)
 
         HOME_URL = "https://www.cwtv.com/"
         episode_metadata_url = "https://images.cwtv.com/feed/mobileapp/video-meta/apiversion_9/guid_"
@@ -45,9 +46,14 @@ class CWtv_Test_Carlos():
 
         for serie in soup_home.find("div",{"class":"footercol fcol1"}).findAll("a"):
 
-            deeplink = HOME_URL + serie.get("href")
+            deeplink = HOME_URL + serie.get("href")[1:]
 
-            guid_serie = requests.get(deeplink).url.split("?play=")[1]
+            try:
+                guid_serie = requests.get(deeplink).url.split("?play=")[1]
+            except IndexError:
+                print(f"{deeplink} no contiene capítulos")
+                self.skippedTitles += 1
+                continue
 
             #obtiene json con ficha técnica que contiene un poco más de info de la serie
             json_serie = Datamanager._getJSON(self, episode_metadata_url+guid_serie)["video"]
@@ -90,106 +96,70 @@ class CWtv_Test_Carlos():
                 'Download'          : None,
                 'IsOriginal'        : None,
                 'IsAdult'           : None,
-                'Packages'          : [{"Type":"free i dunno"}],
+                'Packages'          : [{"Type":"free-vod"}],
                 'Country'           : None,
                 'Timestamp'         : datetime.now().isoformat(),
                 'CreatedAt'         : self._created_at
             }
-            self._append_to_list(payload, listPayload)
-#        Datamanager._checkDBandAppend(self, payload, listDBMovie, listPayload)
+            #self._append_to_list(payload, listPayload)
+            Datamanager._checkDBandAppend(self, payload, listDBMovie, listPayload)
 
             season_container = soup_serie.find("section", {"id":"videosandtouts"}).findAll("div")[0]
 
-            if "no-seasons" in season_container.find("div", {"id":"show-seasons"}).get("class"):
-                for episode_link in season_container.find("ul",{"id":"list_1"}).findAll("a", {"class":"thumbLink"}):
-                    episode_link = episode_link.get("href")
+            for episode_link in season_container.find("ul",{"id":"list_1"}).findAll("a", {"class":"thumbLink"}):
+                episode_link = episode_link.get("href")
+                try:
                     guid_epi = episode_link.split("?play=")[1]
+                except IndexError:
+                    print("Not a valid link, doesn't contain slug.")
+                    continue
 
-                    json_epi = Datamanager._getJSON(self, episode_metadata_url+guid_epi)["video"]
+                json_epi = Datamanager._getJSON(self, episode_metadata_url+guid_epi)["video"]
 
-                    payloadEpi = {
-                        'PlatformCode'  : self._platform_code,
-                        'ParentId'      : id_,
-                        'ParentTitle'   : title,
-                        'Id'            : json_epi["tms_id"],
-                        'Title'         : json_epi["title"],
-                        #'SeasonName'    : seasonName,
-                        'Episode'       : int(json_epi["episode"][1:]),
-                        'Season'        : int(json_epi["season"]),
-                        'Year'          : None,
-                        'Duration'      : int(json_epi["duration_secs"])/60,
-                        'Deeplinks'     : {
-                            'Web': episode_link,
-                            'Android': None,
-                            'iOS': None
-                        },
-                        'Synopsis'      : json_epi["description_long"],
-                        'Rating'        : json_epi["rating"],
-                        'Provider'      : None,
-                        'Genres'        : [json_epi["comscore_genre"]],
-                        'Cast'          : None,
-                        'Directors'     : None,
-                        'Availability'  : None,
-                        'Download'      : None,
-                        'IsOriginal'    : None,
-                        'IsAdult'       : None,
-                        'Country'       : None,
-                        'Packages'      : [{"i dunno"}],
-                        'Timestamp'     : datetime.now().isoformat(),
-                        'CreatedAt'     : self._created_at
-                    }
-                    self._append_to_list(payloadEpi, listPayloadEpi)
-                    #Datamanager._checkDBandAppend(self, payloadEpi, listDBEpi, listPayloadEpi, isEpi=True)
-            else:
-                for season_menu in season_container.find("div", {"id":"show-seasons-menu2"}).findAll("li"):
-                    soup_season = Datamanager._getSoup(self,HOME_URL + season_menu.find("a").get("href"))
-                    for episode_link in soup_season.find("section", {"id":"videosandtouts"}).findAll("div", recursive=False)[0].findAll("a", {"class":"thumbLink"}):
-                        print("\n\n\n", len(soup_season.find("section", {"id":"videosandtouts"}).findAll("div", recursive=False)[0].findAll("a", {"class":"thumbLink"})), "\n\n\n")
-                        episode_link = episode_link.get("href")
-                        guid_epi = episode_link.split("?play=")[1]
+                payloadEpi = {
+                    'PlatformCode'  : self._platform_code,
+                    'ParentId'      : id_,
+                    'ParentTitle'   : title,
+                    'Id'            : guid_epi,
+                    'ExternalIds'   : [{'Provider': 'tms', 'Id': json_epi["tms_id"]}],
+                    'Title'         : json_epi["title"],
+                    #'SeasonName'    : seasonName,
+                    'Episode'       : int(json_epi["episode"][1:]),
+                    'Season'        : int(json_epi["season"]),
+                    'Year'          : None,
+                    'Duration'      : int(json_epi["duration_secs"])//60,
+                    'Deeplinks'     : {
+                        'Web': HOME_URL+(episode_link[1:]),
+                        'Android': None,
+                        'iOS': None
+                    },
+                    'Synopsis'      : json_epi["description_long"],
+                    'Rating'        : json_epi["rating"],
+                    'Provider'      : None,
+                    'Genres'        : [json_epi["comscore_genre"]],
+                    'Cast'          : None,
+                    'Directors'     : None,
+                    'Availability'  : json_epi["expire_time"].split("T")[0],
+                    'Download'      : None,
+                    'IsOriginal'    : None,
+                    'IsAdult'       : None,
+                    'Country'       : None,
+                    'Packages'      : [{"Type":"free-vod"}],
+                    'Timestamp'     : datetime.now().isoformat(),
+                    'CreatedAt'     : self._created_at
+                }
+                #self._append_to_list(payloadEpi, listPayloadEpi)
+                Datamanager._checkDBandAppend(self, payloadEpi, listDBEpi, listPayloadEpi, isEpi=True)
 
-                        json_epi = Datamanager._getJSON(self, episode_metadata_url+guid_epi)["video"]
+        Datamanager._insertIntoDB(self,listPayload,self.titanScraping)
+        Datamanager._insertIntoDB(self,listPayloadEpi,self.titanScrapingEpisodios)
 
-                        payloadEpi = {
-                            'PlatformCode'  : self._platform_code,
-                            'ParentId'      : id_,
-                            'ParentTitle'   : title,
-                            'Id'            : json_epi["tms_id"],
-                            'Title'         : json_epi["title"],
-                            #'SeasonName'    : seasonName,
-                            'Episode'       : int(json_epi["episode"][1:]),
-                            'Season'        : int(json_epi["season"]),
-                            'Year'          : None,
-                            'Duration'      : int(json_epi["duration_secs"])/60,
-                            'Deeplinks'     : {
-                                'Web': episode_link,
-                                'Android': None,
-                                'iOS': None
-                            },
-                            'Synopsis'      : json_epi["description_long"],
-                            'Rating'        : json_epi["rating"],
-                            'Provider'      : None,
-                            'Genres'        : [json_epi["comscore_genre"]],
-                            'Cast'          : None,
-                            'Directors'     : None,
-                            'Availability'  : None,
-                            'Download'      : None,
-                            'IsOriginal'    : None,
-                            'IsAdult'       : None,
-                            'Country'       : None,
-                            'Packages'      : [{"i dunno"}],
-                            'Timestamp'     : datetime.now().isoformat(),
-                            'CreatedAt'     : self._created_at
-                        }
-                        self._append_to_list(payloadEpi, listPayloadEpi)
-                        #Datamanager._checkDBandAppend(self, payloadEpi, listDBEpi, listPayloadEpi, isEpi=True)
-
-        #Datamanager._insertIntoDB(self,listPayload,self.titanScraping)
-        #Datamanager._insertIntoDB(self,listPayloadEpi,self.titanScrapingEpisodios)
-        with open("series.json", "w"):
-            json.dumps(listPayload)
-        with open("episodes.json", "w"):
-            json.dumps(listPayload)
+        """
+        with open("series.json", "w") as f:
+            json.dump({"series":listPayload}, f, indent=2)
+        with open("episodes.json", "w") as f:
+            json.dump({"episodes":listPayloadEpi}, f, indent=2)
+        """
 
         self.sesion.close()
         if not testing:
