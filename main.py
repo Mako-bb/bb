07108df6149d36c2ba00a7b1683999879a37971f
Tomas Cowes <tc@bb.vision>
2021-02-22 17:48:11 -0300

    Modifico multiplatform->amc_networks y agrego doc

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ from platforms.tvland               import TvLand
 from platforms.amazon_login         import AmazonLogin
 from platforms.abc                  import Abc
 from platforms.bet_test             import BetTest
-from platforms.multiplatform        import MultiplatformScraping
+from platforms.amc_networks        import AMCNetworks
 from platforms.syfy import Syfy
 from platforms.logo import Logo
 from platforms.roku_channel         import RokuChannel
