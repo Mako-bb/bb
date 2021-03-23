760c9355dad2a5df8675c9d3f5f6f793878498a8
Tadeo Soresi <tadeosoresi23@outlook.com>
2021-03-22 21:54:03 -0300

    WeTV casi finalizado

--- platforms/wetv.py
+++ platforms/wetv.py
@@ -27,8 +27,7 @@ class WeTV():
 
         Duración: Dependiendo la pc, aprox. 3 minutos.
 
-        Cosas por hacer: Externalizar urls, manejar mejor los errores, 
-        chequear todos los datos, exprimir la plataforma en busca de mas datos.
+        Cosas por hacer: Externalizar urls.
     '''
     def __init__(self, ott_site_uid, ott_site_country, type):
         self._config                = config()['ott_sites'][ott_site_uid]
@@ -46,7 +45,7 @@ class WeTV():
             self._scraping()
 
         if type == 'testing':
-            self._scraping(testing = True)
+            self._scraping(testing=True)
         
         if type == 'return':
             '''
@@ -72,11 +71,10 @@ class WeTV():
 
         self.sesion.close()
 
-        #Upload(self._platform_code, self._created_at, testing = testing)
+        Upload(self._platform_code, self._created_at, testing = testing)
         
 
     def get_principal_info(self, url):
-
         '''Esta funcion extrae la data de la api principal (y mas facil de detectar),
         desde la url en _scraping, paralelamente llama a otras funciones para extraer
         datos restantes'''
@@ -97,6 +95,7 @@ class WeTV():
                                 a['properties']['cardData']['text'].get('description', None),
                                 a['properties']['cardData'].get('images', None),
                                 link,
+                                self.get_seasons(link),
                                 self.get_cast(link_to_cast[2], link_to_cast[3])]) #Cast se extrae de otra api (función aparte)
                                                               
 
@@ -104,19 +103,16 @@ class WeTV():
                 print(f'Error al extraer los datos.')
                 pass
 
-        pandas = pd.DataFrame(series_info, columns=['ID', 'Title', 'Descripción', 'Image', 'Url', 'Season'])
-        #pandas.to_excel("series.xlsx",sheet_name='series') Si queremos previsualizar datos
-        print(pandas)
-
-        links = pandas['Url']
 
         self.payloads(series_info, 'series')
+
+        pandas = pd.DataFrame(series_info, columns=['ID', 'Title', 'Descripción', 'Image', 'Url', 'Seasons', 'Cast'])
+        links = pandas['Url']
 
         self.api_each_serie(links)
 
 
     def get_cast(self, title, _id):
-
         '''Esta función recibe como parametros el title e id y hace el request correspondiente,
             el JSON de esta api esta compuesto por varios indices, el 4 es justamente el que tiene
             data del cast, se calcula el len de este indice (ya que varia para distintas series) y
@@ -142,14 +138,34 @@ class WeTV():
             return None
         else:
             return cast
+    
+    def get_seasons(self, url):
+        '''Funcion que obtiene len de series'''
+
+        content = None
+        try:
+            r = self.sesion.get(f'https://content-delivery-gw.svc.ds.amcn.com/api/v2/content/amcn/wetv/url{url}')
+            data = r.json()
+            try:
+                content = data['data']['children'][3]['properties']['dropdownItems']
+
+            except KeyError as e:
+                print(f'Error, no se encuentra la key {e}, no hay seasons disponibles.')
+                pass
+        except:
+            pass
+        
+        if content:
+            return int(len(content))
+        else:
+            return None
+            
 
     def api_each_serie(self, links):
-
         '''Esta funcion hace request a la api general de cada serie, 
-            las cuales presentan info pero de la ultima season 
-            (la que estaria active en la web, gralmente es la ultima temporada), por lo tanto
-            solo hay info de la temporada active, aun asi encontramos link de la api_episodes
-            que pasariamos a get_pre_episodes.'''
+            las cuales presentan las url de las distintas temporadas,
+            estas url serian usadas por la funcion get_final_episodes
+            para extraer toda la data de cada episodio.'''
 
         list_parameters = []
 
@@ -157,23 +173,31 @@ class WeTV():
             try:
                 r = self.sesion.get(f'https://content-delivery-gw.svc.ds.amcn.com/api/v2/content/amcn/wetv/url{link}')
                 data = r.json()
+
+                _id = [int(s) for s in link.split('--') if s.isdigit()]
                 try:
-                    list_parameters.append(data['data']['children'][2]['properties']['tabs'][0]['permalink']) #Extrae parametro
+                    #list_parameters.append(data['data']['children'][2]['properties']['tabs'][0]['permalink']) #Extrae parametro
+                    content = data['data']['children'][3]['properties']['dropdownItems']
+                    for data in content:
+                        list_parameters.append([_id[0],
+                                    data['properties'].get('nid', None),
+                                    data['properties'].get('title', None),
+                                    data['properties'].get('permalink', None)])
 
                 except Exception as e:
-                    print('Error al extraer datos.')
+                    print(f'Error al extraer datos de {r.url}, no cuenta con episodios.')
                     pass
+
             except Exception as e:
                 print(e)
                 pass
+        
+        
+        self.get_final_episodes(list_parameters)
 
-        self.get_pre_episodes(list_parameters)
+    '''def get_pre_episodes(self, parameters):
 
-    def get_pre_episodes(self, parameters):
-
-        '''Nuevamente la api_episodes sigue mostrando data de la ultima temporada,
-            pero ahora encontramos los links (en dropdownItems) de las demas temporadas,
-            si es que las hay'''
+        FUNCIÓN REEMPLAZADA POR GET EPISODES.
 
         list_parameters = []
 
@@ -190,43 +214,53 @@ class WeTV():
                 pass
         
         
-        self.get_final_episodes(list_parameters)
+        self.get_final_episodes(list_parameters)'''
 
     def get_final_episodes(self, parameters):
-        
         '''Finalmente esta función es la que obtiene los datos de todas las seasons'''
 
         list_final_episodes = []
 
         for parameter in parameters:
             try:
-                r = requests.get(f'https://content-delivery-gw.svc.ds.amcn.com/api/v2/content/amcn/wetv/url{parameter}')
+                r = requests.get(f'https://content-delivery-gw.svc.ds.amcn.com/api/v2/content/amcn/wetv/url{parameter[3]}')
                 data = r.json()
+
                 content = data['data']['children'][4]['children']
+                titles = data['data']['children'][1]['properties']['cardData']['text']
+
                 for a in content:
 
                     season_episode = a['properties']['cardData']['text'].get('seasonEpisodeNumber', None)
 
-                    list_final_episodes.append([a['properties']['cardData']['meta'].get('nid', None),
+                    list_final_episodes.append([parameter[0], #Id serie
+                                                titles.get('showTitle', None), #Titulo serie
+                                                parameter[1], #Id season
+                                                parameter[2], #Title Season
+                                                titles.get('description', None), #Descripcion season
+                                                parameter[3], #Link season
+                                                a['properties']['cardData']['meta'].get('nid', None), #Id. epi
+                                                a['properties']['cardData']['text'].get('title', None), #Title epi
+                                                a['properties']['cardData']['text'].get('description', None),
                                                 a['properties']['cardData']['meta'].get('permalink', None),
                                                 a['properties']['cardData'].get('images', None),
-                                                a['properties']['cardData']['text'].get('title', None),
-                                                a['properties']['cardData']['text'].get('description', None),
                                                 season_episode.split(',')[0].replace('S', '') if season_episode else None,
-                                                season_episode.split(',')[1].replace('E', '') if season_episode else None])                  
+                                                season_episode.split(',')[1].replace('E', '') if season_episode else None,
+                                                int(len(content))]) #len de contenidos (cada contenido es un episode)              
             except KeyError as e:
                 print(f'Error campo no encontrado {e} en {r.url}')
                 pass
         
-        pandas = pd.DataFrame(list_final_episodes, columns=['ID', 'Link', 'Image', 'Title', 'Descripcion', 'Season', 'Episode'])
-        #pandas.to_excel("series.xlsx",sheet_name='series') #Si queremos previsualizar datos
-        print(pandas)
+        #pandas = pd.DataFrame(list_final_episodes, columns=['ID_serie', 'Title_serie', 'Id_season', 'Title_season', 'Description_season',
+                                                            #'Link_season', 'Id_cap', 'Title_cap', 'Descripcion', 'Link_cap', 
+                                                            #'Image', 'Season', 'Episode','Episodes_per_season'])
+        #pandas.to_excel("series.xlsx",sheet_name='series') Si queremos previsualizar datos
+
 
         self.payloads(list_final_episodes, 'episodes')
 
     
     def payloads(self, list_info, _type=None):
-        
         '''Funcion payloads'''
 
         payloads = []
@@ -245,35 +279,36 @@ class WeTV():
             for serie in list_info:
 
                 payload = {
-                    'PlatformCode':  self._platform_code,
-                    'Id':            str(serie[0]),
-                    'Title':         serie[1],
-                    'OriginalTitle': None,
-                    'CleanTitle':    _replace(serie[1]),
-                    'Type':          'serie',
-                    'Year':          None,
-                    'Duration':      None,
-                    'Deeplinks': {
-                        'Web':       'https://www.wetv.com' + serie[4],
-                        'Android':   None,
-                        'iOS':       None,
-                        },
-                    'Playback':      None,
-                    'Synopsis':      serie[2],
-                    'Image':         serie[3],
-                    'Rating':        None,
-                    'Provider':      None,
-                    'Genres':        None,
-                    'Cast':          serie[5],
-                    'Directors':     None,
-                    'Availability':  None,
-                    'Download':      None,
-                    'IsOriginal':    None,
-                    'IsAdult':       None,
-                    'Packages':      packages,
-                    'Country':       None,
-                    'Timestamp':     datetime.now().isoformat(),
-                    'CreatedAt':     self._created_at
+                                    'PlatformCode':  self._platform_code,
+                                    'Id':            str(serie[0]),
+                                    'Title':         serie[1],
+                                    'OriginalTitle': None,
+                                    'CleanTitle':    _replace(serie[1]),
+                                    'Type':          'serie',
+                                    'Year':          None,
+                                    'Duration':      None,
+                                    'Seasons':      serie[5],
+                                    'Deeplinks': {
+                                        'Web':       'https://www.wetv.com' + serie[4],
+                                        'Android':   None,
+                                        'iOS':       None,
+                                        },
+                                    'Playback':      None,
+                                    'Synopsis':      serie[2],
+                                    'Image':         [serie[3]],
+                                    'Rating':        None,
+                                    'Provider':      None,
+                                    'Genres':        None,
+                                    'Cast':          serie[6],
+                                    'Directors':     None,
+                                    'Availability':  None,
+                                    'Download':      None,
+                                    'IsOriginal':    None,
+                                    'IsAdult':       None,
+                                    'Packages':      packages,
+                                    'Country':       None,
+                                    'Timestamp':     datetime.now().isoformat(),
+                                    'CreatedAt':     self._created_at
                     }
 
                 Datamanager._checkDBandAppend(self, payload, list_db_series, payloads)
@@ -286,39 +321,59 @@ class WeTV():
 
             for epi in list_info:
 
-                payload_epi = {
-                                    'PlatformCode'  : self._platform_code,
-                                    'ParentId'      : None,
-                                    'ParentTitle'   : None,
-                                    'Id'            : str(epi[0]),
-                                    'Title'         : epi[3],
-                                    'OriginalTitle' : None,
-                                    'CleanTitle'    : _replace(epi[3]),
-                                    'Type':          'serie',
-                                    'Episode'       : epi[6],
-                                    'Season'        : epi[5],
-                                    'Year'          : None,
-                                    'Duration'      : None,
-                                    'Deeplinks'     : {
-                                        'Web': 'https://www.wetv.com' + epi[1],
-                                        'Android': None,
-                                        'iOS': None
-                                    },
-                                    'Synopsis'      : epi[4],
-                                    'Rating'        : None,
-                                    'Provider'      : None,
-                                    'Genres'        : None,
-                                    'Cast'          : None,
-                                    'Directors'     : None,
-                                    'Availability'  : None,
-                                    'Download'      : None,
-                                    'IsOriginal'    : None,
-                                    'IsAdult'       : None,
-                                    'Country'       : None,
-                                    'Packages'      : packages,
-                                    'Timestamp'     : datetime.now().isoformat(),
-                                    'CreatedAt'     : self._created_at
+                payload_epi =   {      
+                                    "PlatformCode":  self._platform_code,      
+                                    "Id":            int(epi[6]), 
+                                    "ParentId":      int(epi[0]), 
+                                    "ParentTitle":   epi[1],  
+                                    "Episode":       int(epi[12]) if epi[12] else None,   
+                                    "Season":        int(epi[11]) if epi[11] else None, 
+                                    "Seasons":       [ 
+                                                        {
+                                                        "Id": epi[2],           
+                                                        "Synopsis": epi[4],     
+                                                        "Title": epi[1] + ' ' + epi[3],        
+                                                        "Deeplink":  'https://www.wetv.com' + epi[5] if epi[5] else None,    
+                                                        "Number": None,       
+                                                        "Year": None,         
+                                                        "Image": None, 
+                                                        "Directors": None,   
+                                                        "Cast": None,       
+                                                        "Episodes": int(epi[13]) if epi[13] else None,      
+                                                        "IsOriginal": None    
+                                                        },
+                        
+                                    ],
+                                    "Title":         epi[7],      
+                                    "CleanTitle":    _replace(epi[7]),      
+                                    "OriginalTitle": epi[7],                          
+                                    "Type":          'serie',           
+                                    "Year":          None,          
+                                    "Duration":      None,      
+                                    "ExternalIds":   None,      
+                                    "Deeplinks": {          
+                                        "Web":       'https://www.wetv.com' + epi[9] if epi[9] else None,                 
+                                        "Android":   None,          
+                                        "iOS":       None,      
+                                    },      
+                                    "Synopsis":      epi[8],      
+                                    "Image":         ['https://www.wetv.com' + epi[10]] if epi[10] else None,      
+                                    "Rating":        None,           
+                                    "Provider":      None,      
+                                    "Genres":        None,          
+                                    "Cast":          None,      
+                                    "Directors":     None,          
+                                    "Availability":  None,           
+                                    "Download":      None,      
+                                    "IsOriginal":    None,          
+                                    "IsAdult":       None,    
+                                    "IsBranded":     None,    
+                                    "Packages":      packages,          
+                                    "Country":       None,      
+                                    "Timestamp":     datetime.now().isoformat(),       
+                                    "CreatedAt":     self._created_at, 
                                 }
+
 
                 Datamanager._checkDBandAppend(self, payload_epi, list_db_episodes, payloads)
 
