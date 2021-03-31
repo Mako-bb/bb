f429f38a782856496fb17ffd0f11bd11411729fb
Tadeo Soresi <tadeosoresi23@outlook.com>
2021-03-30 23:14:57 -0300

    WeTV a master

--- platforms/wetv.py
+++ platforms/wetv.py
@@ -25,9 +25,7 @@ class WeTV():
             la data de la ultima temporada de cada serie (ya que en la web se muestran las ultimas temporadas como novedades), 
             por lo tanto hay que visitar multiples URL's de las restantes temporadas.
 
-        Duración: Dependiendo la pc, aprox. 3 minutos.
-
-        Cosas por hacer: mover seasons de episodes a series.
+        Duración: Dependiendo la pc, aprox. 5 minutos.
     '''
 
     def __init__(self, ott_site_uid, ott_site_country, type):
@@ -91,28 +89,86 @@ class WeTV():
                 link = a['properties']['cardData']['meta'].get(
                     'permalink', None)
 
-                series_info.append((a['properties']['cardData']['meta'].get('nid', None),
+                series_info.append([a['properties']['cardData']['meta'].get('nid', None),
                                     a['properties']['cardData']['text'].get('title', None),
                                     a['properties']['cardData']['text'].get('description', None),
                                     a['properties']['cardData'].get('images', None),
                                     link,
-                                    self.get_seasons(link),
-                                    self.get_cast(link_to_cast[2], link_to_cast[3])))  # Cast se extrae de otra api (función aparte)
+                                    self.get_seasons(link), 
+                                    self.get_cast(link_to_cast[2], link_to_cast[3])])  # Cast se extrae de otra api (función aparte)
 
             except KeyError:
                 print(f'Error al extraer los datos.')
                 pass
         
-        series_info2 = set(series_info)
-        series_info = [list(serie) for serie in series_info2]
+        series_info_final = []
 
-        self.payloads(series_info, 'series')
+        for serie in series_info:
+            if serie not in series_info_final:
+                series_info_final.append(serie)
+        
+        self.payloads(series_info_final, 'series')
 
-        pandas = pd.DataFrame(series_info, 
+        pandas = pd.DataFrame(series_info_final, 
                                 columns=['ID', 'Title', 'Descripción', 'Image', 'Url', 'Seasons', 'Cast'])
         links = pandas['Url']
 
         self.api_each_serie(links)
+
+    def get_seasons(self, url):
+        '''Funcion que obtiene datos de las seasons para cada serie,
+        primero hace un request al link de la serie general, del cual
+        extra los links a todas las seasons (si es que las hay), luego
+        visita cada link de cada season y hace un conteo de episodes.'''
+
+        seasons = []
+        links = []
+
+        try:
+            r = self.sesion.get(self.common_url + url)
+            data = r.json()
+            content = data['data']['children'][3]['properties']['dropdownItems']
+
+            for season in content: #SEASONS DISPONIBLES EN PLATAFORMA!!
+                links.append([season['properties'].get('permalink', None), 
+                                season['properties'].get('nid', None),
+                                season['properties'].get('title', None)])
+
+        except Exception as e:
+            print(f'Ocurrio error al extraer data de seasons {e}, de la url {r.url}, no hay seasons disponibles.')
+            pass
+        
+        if links:
+            for link in links:
+                try:
+                    r = requests.get(self.common_url + link[0])
+                    data = r.json()
+
+                    episodes = len(data['data']['children'][4]['children']) #cant. episodes
+                    title = data['data']['children'][1]['properties']['cardData']['text'].get('showTitle', None)
+                    image = data['data']['children'][1]['properties']['cardData']['images'].get('desktop', None)
+
+                    seasons.append({ #Tomamos parte del payload
+                                "Id": link[1],
+                                "Synopsis": None,
+                                "Title": title + ' ' + link[2] if link[2] else None,
+                                "Deeplink":  'https://www.wetv.com' + link[0] if link[0] else None,
+                                "Number": None,
+                                "Year": None,
+                                "Image": image if image else None,
+                                "Directors": None,
+                                "Cast": None,
+                                "Episodes": int(episodes) if episodes else None,
+                                "IsOriginal": None
+                            })
+
+                except Exception as e:
+                    print(f'Error {e}')
+
+            return seasons
+
+        else:
+            return None
 
     def get_cast(self, title, _id):
         '''Esta función recibe como parametros el title e id y hace el request correspondiente,
@@ -140,30 +196,9 @@ class WeTV():
         if not cast:
             return None
         else:
+            #convertimos lista a strings
             final_cast = ', '.join([str(elem).replace(' &', ',') for elem in cast])
             return final_cast
-
-    def get_seasons(self, url):
-        '''Funcion que obtiene len de series'''
-
-        content = None
-        try:
-            r = self.sesion.get(self.common_url + url)
-            data = r.json()
-            try:
-                content = data['data']['children'][3]['properties']['dropdownItems']
-
-            except KeyError as e:
-                print(
-                    f'Error, no se encuentra la key {e}, no hay seasons disponibles.')
-                pass
-        except:
-            pass
-
-        if content:
-            return int(len(content))
-        else:
-            return None
 
     def api_each_serie(self, links):
         '''Esta funcion hace request a la api general de cada serie, 
@@ -172,7 +207,7 @@ class WeTV():
             para extraer toda la data de cada episodio.'''
 
         list_parameters = []
-        
+
         for link in links:
             try:
                 r = self.sesion.get(self.common_url + link)
@@ -180,7 +215,6 @@ class WeTV():
 
                 _id = [int(s) for s in link.split('--') if s.isdigit()]
                 try:
-                    # list_parameters.append(data['data']['children'][2]['properties']['tabs'][0]['permalink'])
                     content = data['data']['children'][3]['properties']['dropdownItems']
                     for data in content:
                         list_parameters.append([_id[0],
@@ -220,10 +254,10 @@ class WeTV():
 
                     list_final_episodes.append((parameter[0],  # Id serie
                                                 titles.get('showTitle', None), # Titulo serie
-                                                parameter[1],  # Id season
-                                                parameter[2],  # Title Season
-                                                titles.get('description', None), # Descripcion season
-                                                parameter[3],  # Link season
+                                                parameter[1],  # Id season(sin usar)
+                                                parameter[2],  # Title Season (sin usar)
+                                                titles.get('description', None), # Descripcion season (sin usar)
+                                                parameter[3],  # Link season (sin usar)
                                                 episode_data.get('nid', None),  # Id. epi
                                                 episode_data2.get('title', None),  # Title epi
                                                 episode_data2.get('description', None), #Descripcion epi
@@ -233,8 +267,8 @@ class WeTV():
                                                 season_episode.split(',')[0].replace(
                                                     'S', '') if season_episode else None,
                                                 season_episode.split(',')[1].replace(
-                                                    'E', '') if season_episode else None,
-                                                int(len(content))))  # len de contenidos (cada contenido es un episode)
+                                                    'E', '') if season_episode else None))  
+                                
             except KeyError as e:
                 print(f'Error campo no encontrado {e} en {r.url}')
                 pass
@@ -270,7 +304,7 @@ class WeTV():
                     'Type':          'serie',
                     'Year':          None,
                     'Duration':      None,
-                    'Seasons':      serie[5],
+                    "Seasons":       serie[5],
                     'Deeplinks': {
                         'Web':       'https://www.wetv.com' + serie[4],
                         'Android':   None,
@@ -312,24 +346,8 @@ class WeTV():
                     "ParentTitle":   epi[1],
                     "Episode":       int(epi[12]) if epi[12] else None,
                     "Season":        int(epi[11]) if epi[11] else None,
-                    "Seasons":       [
-                        {
-                            "Id": epi[2],
-                            "Synopsis": epi[4],
-                            "Title": epi[1] + ' ' + epi[3],
-                            "Deeplink":  'https://www.wetv.com' + epi[5] if epi[5] else None,
-                            "Number": None,
-                            "Year": None,
-                            "Image": None,
-                            "Directors": None,
-                            "Cast": None,
-                            "Episodes": int(epi[13]) if epi[13] else None,
-                            "IsOriginal": None
-                        },
-
-                    ],
                     "Title":         epi[7],
-                    "CleanTitle":    _replace(epi[7]),
+                    "CleanTitle":    _replace(epi[7]) if epi[7] else None,
                     "OriginalTitle": epi[7],
                     "Type":          'serie',
                     "Year":          None,
@@ -397,6 +415,7 @@ class WeTV():
         elif r.status_code == 503:
             print(
                 f'Error {r.status_code}, el servidor no pudo procesar la petición.')
-
+                
         else:
             print(f'Error {r.status_code}.')
+
