01a77037d69b2790be248ea046a8b216a8297071
ArielGabrielPeralta <ariel_peralta_bb@outlook.es>
2021-05-31 16:29:30 -0300

    Mi primer commit

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ from platforms.cwtv import CWtv
 import argparse
 import logging
 
-
-from platforms.pongalo              import Pongalo
-from platforms.cinema_uno           import CinemaUno
-from platforms.acorntv              import AcornTV
-from platforms.adultswim            import AdultSwim
-from platforms.flixfling            import FlixFling
-from platforms.trutv                import TruTV
-from platforms.quibi                import Quibi
-from platforms.optimum              import Optimum
+from platforms.pongalo import Pongalo
+from platforms.cinema_uno import CinemaUno
+from platforms.acorntv import AcornTV
+from platforms.adultswim import AdultSwim
+from platforms.flixfling import FlixFling
+from platforms.trutv import TruTV
+from platforms.quibi import Quibi
+from platforms.optimum import Optimum
 # from platforms.acorntv_test         import AcornTV_Test
-from platforms.cwtv                 import CWtv
-from common                         import config
-from platforms.freeform             import Freeform
-from platforms.myoutdoortv          import MyOutdoorTV
-from platforms.cwseed               import CwSeed
-from platforms.pantaya              import Pantaya
-from platforms.cartoonnetwork       import CartoonNetwork
-from platforms.fandango             import FandangoNOW
-from platforms.boomerang            import Boomerang
-from platforms.optimum_test         import OptimumTest
-from platforms.amazon_login         import AmazonLogin
-from platforms.abc                  import Abc
+from platforms.cwtv import CWtv
+from common import config
+from platforms.freeform import Freeform
+from platforms.myoutdoortv import MyOutdoorTV
+from platforms.cwseed import CwSeed
+from platforms.pantaya import Pantaya
+from platforms.cartoonnetwork import CartoonNetwork
+from platforms.fandango import FandangoNOW
+from platforms.boomerang import Boomerang
+from platforms.optimum_test import OptimumTest
+from platforms.amazon_login import AmazonLogin
+from platforms.abc import Abc
+from platforms.pluto import Pluto
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -43,10 +43,10 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     new_site_choices = list(config()['ott_sites'].keys())
-    parser.add_argument('--c',        help='País para Scrapear', type=str)
-    parser.add_argument('--o',        help='Operación', type=str)
-    parser.add_argument('--date',     help = 'Fecha del log buscado', type = str)
-    parser.add_argument('ott_site',   help='Sitios Para Scrapear',
+    parser.add_argument('--c', help='País para Scrapear', type=str)
+    parser.add_argument('--o', help='Operación', type=str)
+    parser.add_argument('--date', help='Fecha del log buscado', type=str)
+    parser.add_argument('ott_site', help='Sitios Para Scrapear',
                         type=str, choices=new_site_choices)
     args = parser.parse_args()
 
@@ -62,6 +62,7 @@ if __name__ == '__main__':
         platform_code = config()['ott_sites'][ott_platforms]["countries"].get(ott_site_country)
         if platform_code:
             from analysis.utils.excel_template import ExcelTemplate
+
             ExcelTemplate.export_excel(platform_code, logat)
         else:
             print(f"Error in PlatformCode. See config.yaml")
