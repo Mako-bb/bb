9bdd08be73d0848d30c3d2b3da62634f3f4eb7f3
matikrapf <mk@bb.vision>
2021-06-05 16:41:03 -0300

    config.yaml small changes

---
+++ platforms/pluto_mk.py
@@ -0,0 +1,49 @@
+import time
+from pymongo.message import insert
+import requests
+import ast
+from handle.replace import _replace
+from common import config
+from handle.mongo import mongo
+from updates.upload         import Upload
+# from time import sleep
+# import re
+
+class Starz_mk():
+    """
+    """
+    def __init__(self, ott_site_uid, ott_site_country, type):
+        self.ott_site_uid = ott_site_uid
+        self.ott_site_country = ott_site_country
+        self._config = config()['ott_sites'][ott_site_uid]
+        self._platform_code = self._config['countries'][ott_site_country]
+        # self._start_url             = self._config['start_url']
+        self._created_at = time.strftime("%Y-%m-%d")
+        self.mongo = mongo()
+        self.titanPreScraping = config()['mongo']['collections']['prescraping']
+        self.titanScraping = config()['mongo']['collections']['scraping']
+        self.titanScrapingEpisodios = config()['mongo']['collections']['episode']
+
+        self.api_url = self._config['api_url']
+
+        self.session = requests.session()
+
+        if type == 'return':
+            '''
+            Retorna a la Ultima Fecha
+            '''
+            params = {"PlatformCode": self._platform_code}
+            lastItem = self.mongo.lastCretedAt(self.titanScraping, params)
+            if lastItem.count() > 0:
+                for lastContent in lastItem:
+                    self._created_at = lastContent['CreatedAt']
+            self._scraping()
+
+        if type == 'scraping':
+            self._scraping()
+
+        if type == 'testing':
+            self._scraping(testing=True)
+    
+    def _scraping(self, testing=False):
+        print('ok')
