c1fa4d95b836c019ba6b23feca1001eb661c4933
Tomas Aiello <aiello.tomas@gmail.com>
2021-06-07 16:48:41 -0300

    script de starz

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from platforms.amazon_login import AmazonLogin
 
 from platforms.pluto_tomas import Pluto_tomas
 
-from platforms.pluto_ariel import Pluto_ari
+
 from platforms.pluto_mv import Pluto_mv
 from platforms.optimum_test import OptimumTest
 from platforms.boomerang import Boomerang
@@ -27,7 +27,7 @@ from platforms.trutv import TruTV
 from platforms.quibi import Quibi
 from platforms.optimum import Optimum
 #from platforms.acorntv_test         import AcornTV_Test
-from platforms.pluto_ariel import Pluto
+from platforms.pluto_ariel import Pluto_ari
 from platforms.cwtv import CWtv
 from common import config
 from platforms.freeform import Freeform
@@ -43,13 +43,9 @@ from platforms.abc import Abc
 from platforms.hbo_prueba import HboPrueba
 from platforms.pluto_capacitacion import PlutoCapacitacion
 from platforms.pluto_ggarcia import Pluto_gg
-<<<<<<< HEAD
 from platforms.hbo_prueba            import HboPrueba
 from platforms.starz_tom import Starz
 from platforms.pluto_mv import Pluto_mv
-=======
-from platforms.pluto_mk import Pluto_mk
->>>>>>> 85e4eb6d55f9fd1010444e451424e1e43adeaf50
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
--- platforms/starz_tom.py
+++ platforms/starz_tom.py
@@ -7,9 +7,11 @@ from handle.mongo import mongo
 from pprint import pprint 
 from bs4 import BeautifulSoup
 import json
+from updates.upload         import Upload
 
 
 class Starz():
+
     def __init__(self, ott_site_uid, ott_site_country, type):
 
         self._config = config()['ott_sites'][ott_site_uid]
@@ -38,7 +40,34 @@ class Starz():
     def _scraping(self, testing = False ):
 
         items = self._request_parser(self.api_url)
-        self._payloads(items)  
+        payload_list = self._payloads(items)
+        
+
+        self.scraped = self.query_field(self.titanScraping, field='Id')
+        # self.scraped_episodes = self.query_field(self.titanScrapingEpisodios, field='Id')
+
+        # TODO: Aprender Datamanager
+        # scraped = Datamanager._getListDB(self,self.titanScraping)
+        # scraped_episodes = Datamanager._getListDB(self,self.titanScrapingEpisodios)
+
+        # Listas con contenidos y episodios dentro (DICT):
+        self.payloads = []
+        self.episodes_payloads = []
+
+        if payload_list:
+            self.mongo.insertMany(self.titanScraping, payload_list)
+        #if self.episodes_payloads:
+        #     self.mongo.insertMany(self.titanScrapingEpisodios, self.episodes_payloads)
+
+
+        # Validar tipo de datos de mongo:
+        Upload(self._platform_code, self._created_at, testing=True)
+
+        print("Fin")
+    
+        
+
+        #self.mongo.insertMany(self.titanScraping, self.payloads)
 
         
     def _request_parser(self, uri):
@@ -53,23 +82,108 @@ class Starz():
     def _payloads(self, items):
         
         items_list = items['playContentArray']['playContents']
-        payload = {}
+        payload_list = []
         for item_list in items_list:
+            payload = {}
             payload['Title'] = item_list['title']
             payload['ID'] = item_list['contentId']
             payload['Type'] = item_list['contentType']
-            payload['Cast'] = self._get_cast(item_list['actors'])
-            print(payload['Cast'])
+            payload['Credits'] = self._get_credits(item_list['credits'])
+            payload['Log Line'] = item_list['logLine']
 
-
-
+            if payload['Type'] == "Series with Season" :
+                payload['isSerie'] = self._get_data_serie(item_list['childContent'])
+                payload['isSerie'] = item_list['episodeCount']
+            
+            try:
+                payload['Actors'] = self._get_cast(item_list['actors'])
+            except: 
+                KeyError
+            payload_list.append(payload)
+        return payload_list
+         
+            
     def _get_cast (self, items):
 
         for item in items:
-            payload = {}
-            payload['Name'] = item['fullName']
-            payload['ID'] = item['id']
+            payload = {
+            'Name' : item['fullName'],
+            'ID' : item['id']
+            }
         return payload 
+
+    def _get_credits(self, items):
+
+        for item in items:
+            payload = {}
+            try :
+                payload['Name'] = item['name']
+                payload['Rol'] = item['keyedRoles'][0]['name']
+                payload['ID'] = item['id']
+
+            except:
+                KeyError
+            
+        return payload 
+
+    def _get_data_serie(self, items):
+
+        for item in items:
+            payload = {
+                'Season Title' : item['childContent'][0]['title'],
+                'ID' : item['childContent'][0]['contentId'],
+                'Log Line' : item['logLine'],
+                'Episodes' :self._get_episodes(item['childContent']),
+                'Credits' : self._get_credits(item),
+                'Genres' : item['childContent'][0]['genres'],
+            }
+        return payload 
+    
+    def _get_episodes(self, items):
+
+        for item in items:
+            payload = {
+                "Title" : item['title'],
+                'ID' : item['contentId'],
+                'Logline' : item['logLine'],
+                'isHD' : item['HD'],
+                #'Audio Type' : item['audioType'],
+                #'Downloadable' : item['downloadable']
+            }
+        return payload
+
+    def query_field(self, collection, field=None):
+        """Método que devuelve una lista de una columna específica
+        de la bbdd.
+
+        Args:
+            collection (str): Indica la colección de la bbdd.
+            field (str, optional): Indica la columna, por ejemplo puede ser
+            'Id' o 'CleanTitle. Defaults to None.
+
+        Returns:
+            list: Lista de los field encontrados.
+        """
+        find_filter = {
+            'PlatformCode': self._platform_code,
+            'CreatedAt': self._created_at
+        }
+
+        find_projection = {'_id': 0, field: 1, } if field else None
+
+        query = self.mongo.db[collection].find(
+            filter=find_filter,
+            projection=find_projection,
+            no_cursor_timeout=False
+        )
+
+        if field:
+            query = [item[field] for item in query if item.get(field)]
+        else:
+            query = list(query)
+
+        return query
+
 
 
         
