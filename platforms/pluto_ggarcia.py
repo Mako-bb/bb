26a258034361cb8b2962bf264b05eb1157c107df
unknown <gg@bb.vision>
2021-06-08 00:43:15 -0300

    mejore mi código de pluto, pero sigue rompiendo al final (error 404)

--- platforms/pluto_ggarcia.py
+++ platforms/pluto_ggarcia.py
@@ -7,6 +7,8 @@ from time import sleep
 import re
 from datetime               import datetime
 from updates.upload         import Upload
+import pandas
+import numpy
 
 class Pluto_gg():
     """
@@ -38,7 +40,7 @@ class Pluto_gg():
                     self._created_at = lastContent['CreatedAt']
 
             self._scraping()
-            
+            print('se ingresaron todos los contenidos correctamente')
             
 
         if type == 'scraping':
@@ -48,67 +50,83 @@ class Pluto_gg():
             self.request_series
             self._scraping(testing=True)
            
+                    
+    def _scraping(self, testing=False):
+        """Método que realiza el scraping y controla los duplicados """
+        payloads = []
+        payload_episodes = []
+        dict_metadata = {}
+        for categorie in self._get_contents():
+            items_list = categorie['items']
+            for _item in items_list:
+                    payload = self.get_payloads(_item)
+                    if payload in payloads:
+                        print('se pudrio')
+                        continue
+                    else:
+                        payloads.append(payload)
+                        self.mongo.insert(self.titanScraping, payload)
+                        print(f"Se ingreso {payload} correctamente")
+                        if _item['type'] != 'movie': 
+                            episode =  self.get_payloadepisode(_item['slug'])
+                            if episode in payload_episodes:
+                                continue
+                            else:
+                                result = self.get_payloadepisode(_item['slug'])
+                                if result == None:
+                                    continue
+                                payload_episodes.append(episode)
+                                self.mongo.insert(self.titanScrapingEpisodios, episode)
+                                print(f"Se ingreso {episode} correctamente")  
+            
+
+    def _get_contents(self):
+        """Método que trae un diccionario de contenidos """
+        list_categories = self.request().json()
+        dict_contents = list_categories['categories']
+        return dict_contents
+
+
     def request(self): 
        """metodo para hacer una peticion a un servidor"""
 
        uri = self.api_url
        response = self.session.get(uri)
-       return response
-                  
-    def request_series(self, slug):
-          
+       return response  
 
+    def request_series(self, slug):
+        """Método que hace requests y devielve una lista de temporadas
+        Esta funcion rompe en una parte, casi al final de extraer todos los contenidos ###revisar """
         uri_series = self.api_series + slug
         response = self.session.get(uri_series)
-        if response != 200:
+        if response.status_code != 200:
             try:
                 request_timeout = 5
                 response = self.session.get(uri_series, timeout=request_timeout)
-            except requests.exceptions.ConnectionError:
-                print("Connection Error, Retrying")
-                time.sleep(request_timeout)
-
+            except:
+                return None
 
         dict_seasons = response.json()
         seasons = dict_seasons['seasons']
-        for season in seasons:
-            for episodes in season['episodes']:
-                episode = { 
-                    'Id': episodes['_id'],
-                    'Episode': episodes['name'],
-                    'season': episodes['season'],
-
-                }
-            return episode
-        
-    def _scraping(self, testing=False):
-        """Método que realiza el scraping y controla los duplicados (?)"""
-        payloads = []
-        episodes = []
-        dict_metadata = {}
-        list_categories = self.request().json()
-        dict_contents = list_categories['categories']
-        for categorie in dict_contents:
-            items_list = categorie['items']
-            for _item in items_list:
-                payload = self.get_payloads(_item)
-                if payload in payloads:
-                    print('se pudrio')
-                    pass
-                else:
-                    payloads.append(payload)
-                if _item['type'] != 'movie':
-                    episode = self.request_series(_item['slug'])
-                    self.mongo.insert(self.titanScrapingEpisodios, episode)
-                    print(f"Se ingreso {episode} correctamente")    
-                    
-                   
+        return seasons            
             
+    def get_payloadepisode(self, slug):
+        """Método que trae payloads por episodio """
+        season = self.request_series(slug)
+        if season == None:
+            return None
+        for episodes in season['episodes']:
+            payload_episode = {'Id': episodes['_id'],
+                'Episode': episodes['name'],
+                'season': episodes['season'],}
+            return payload_episode
+
+
 
 
     def get_payloads(self, _item):
-        """Metodo que genera payloads por item, y los sube a mongo """ 
-        
+        """Metodo que genera payloads por contenido """ 
+       
             
         payload = {}
         payload["Id"] = _item['_id']
@@ -127,16 +145,12 @@ class Pluto_gg():
                     "iOS": None,
                                 }
         payload['Timestamp']= datetime.now().isoformat()
-        
-        self.mongo.insert(self.titanScraping, payload)
-        print(f"Se ingreso {payload} correctamente")
-                
-          
         return payload
       
 
     
     def get_duration(self, payload):
+        """Método que convierte la duración en horas(?)"""
         if payload == 0:
             return None
         else:
