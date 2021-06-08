fc1228bb5c203c78e0afb486f48b194e285d6325
Juan Cruz <jcr@bb.vision>
2021-06-08 17:52:48 -0300

    upload

--- analysis/utils/excel_template.py
+++ analysis/utils/excel_template.py
@@ -39,11 +39,10 @@ class ExcelTemplate():
         del os
 
         with pd.ExcelWriter("excel_exports/" + file_name) as writer:
-            if df_p.empty:
-                pass
-            else:
+            try:
                 df_p.to_excel(writer, sheet_name='titanPreScraping', encoding="utf-8", index=False)
-
+            except:
+                pass
             df.to_excel(writer, sheet_name='titanScraping', encoding="utf-8", index=False)
             df_episodes.to_excel(writer, sheet_name='titanScrapingEpisodes', encoding="utf-8", index=False)
 
