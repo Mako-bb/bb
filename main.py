d134580a350d9548c57d4e047cee5baac99f94be
unknown <gg@bb.vision>
2021-06-11 09:52:45 -0300

    starz_panda is coming

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from platforms.amazon_login import AmazonLogin
 
 from platforms.pluto_tomas import Pluto_tomas
 
-<<<<<<< HEAD
 from platforms.pluto_ariel import Pluto_ari
 from platforms.pluto_mv import Pluto_mv
-=======
-
-
->>>>>>> 4ce42ce3afc360fe982badd7c18ac2fbeaae56bc
 from platforms.optimum_test import OptimumTest
 from platforms.boomerang import Boomerang
 from platforms.fandango import FandangoNOW
@@ -42,7 +37,6 @@ from platforms.pantaya import Pantaya
 from platforms.cartoonnetwork import CartoonNetwork
 from platforms.fandango import FandangoNOW
 from platforms.boomerang import Boomerang
-<<<<<<< HEAD
 from platforms.optimum_test import OptimumTest
 from platforms.amazon_login import AmazonLogin
 from platforms.abc import Abc
@@ -50,21 +44,7 @@ from platforms.hbo_prueba import HboPrueba
 from platforms.pluto_capacitacion import PlutoCapacitacion
 from platforms.pluto_ggarcia import Pluto_gg
 from platforms.pluto_mk import Pluto_mk
-=======
-from platforms.optimum_test         import OptimumTest
-from platforms.amazon_login         import AmazonLogin
-from platforms.abc                  import Abc
-from platforms.hbo_prueba           import HboPrueba
-from platforms.pluto_capacitacion   import PlutoCapacitacion
-from platforms.pluto_ggarcia        import Pluto_gg
-from platforms.hbo_prueba           import HboPrueba
-
-from platforms.starz_capacitacion   import StarzCapacitacion
-from platforms.pluto_mk import Pluto_mk
-from platforms.starz_mk import Starz_mk
-from platforms.pluto_mv             import Pluto_mv
-from platforms.starz_mv             import Starz_mv
->>>>>>> 4ce42ce3afc360fe982badd7c18ac2fbeaae56bc
+from platforms.starz_panda import Starz_panda
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
