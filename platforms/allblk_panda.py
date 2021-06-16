f24094261454739f56a31a6a6098c4ae4c6366c6
unknown <gg@bb.vision>
2021-06-16 18:35:48 -0300

    allblk in progress

--- platforms/allblk_panda.py
+++ platforms/allblk_panda.py
@@ -9,6 +9,7 @@ import json
 import platform
 from requests import api
 import selenium
+from selenium.webdriver.firefox.webdriver import WebDriver
 from handle.replace     import _replace
 from common             import config
 from datetime           import datetime
@@ -17,10 +18,15 @@ from handle.datamanager import Datamanager
 from updates.upload     import Upload
 from bs4                import BeautifulSoup
 from selenium           import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Allblk_panda:
-
+    """Allblk es una plataforma estadounidense que si bien tiene todos los contenidos en una misma url
+    es necesario ir a cada url de cada película para obtener la info.
+    La información que contiene cada url es el Título, Sinopsis y a veces el cast y el director"""
 
     def __init__(self, ott_site_uid, ott_site_country, type):
         self._config = config()['ott_sites'][ott_site_uid]
@@ -32,6 +38,7 @@ class Allblk_panda:
         self.sesion                 = requests.session()
         self.skippedEpis            = 0
         self.skippedTitles          = 0
+        self.start_url = self._config['start_url']
 
         if type == 'return':
             '''
@@ -69,13 +76,32 @@ class Allblk_panda:
         query = {item[field] for item in query}
         return query
 
+        
+
+          
+    def _get_url(self):
+        """Método que trae una lista de url de cada contenido de la plataforma"""
+        req = self.sesion.get(self.start_url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        contenedor = soup.find_all('a', href=True, itemprop=True)
+        url_list = []
+        for url in contenedor:
+            #print(url)
+            print('Found the URL: ', url['href'])
+            url_list.append(url['href'])
+        return url_list
+    def _request_url(self, url_list):
+        for url in url_list:
+            req = self.sesion.get(url)
+            soup = BeautifulSoup(req.text, 'html.parser')
+            contents = soup.find('meta', content=True, itemprop=True)
+            print(contents['content'])## Con esto puedo saber si es una serie o una pelicula.
+            for content in contents:
+                if content == dict:
+                    print()
+
 
     def _scraping(self, testing=False):
-        req = self.sesion.get('https://allblk.tv/browse/all/')#se que esta hardcodeado, es porque estoy teniendo un problema con el config.yaml
-        soup = BeautifulSoup(req.text, 'html.parser')
-        rows = soup.find_all('div', {'class':'row'}) #buscar bien en las rows
-                                                     #en las <a hay url, quizas si busco por ahi, puedo evitar selenium.
-        
-            
-
-        
+        list_url = self._get_url() 
+        req = self._request_url(list_url)
+        return
