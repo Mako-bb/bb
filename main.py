41083702afd48f3fe9aead8a406658b523110328
Juan Cruz <jcr@bb.vision>
2021-06-23 15:12:15 -0300

    Elimino hbo del main

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ from platforms.trutv import TruTV
 from platforms.quibi import Quibi
 from platforms.optimum import Optimum
 #from platforms.acorntv_test         import AcornTV_Test
-from platforms.pluto_ariel import Pluto_ari
 from platforms.cwtv import CWtv
 from common import config
 from platforms.freeform import Freeform
