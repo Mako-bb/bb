a72704164cbc6e8ff8f0b13abef99c085a90bb7f
Federico Velasco <fev@bb.vision>
2021-06-25 09:17:20 -0300

    avance en hbo

--- platforms/hbo_fev.py
+++ platforms/hbo_fev.py
@@ -48,76 +48,130 @@ class HBO_Fev():
                     self._created_at = lastContent['CreatedAt']
             self._scraping()
 
-    #documentaries_hbo_metadata = dict()
-    #documentaries_hbo_metadata['title'] = ()
-    #documentaries_hbo_metadata['duration'] = ()
-    #documentaries_hbo_metadata['title_cleared'] = ()
-    #documentaries_hbo_metadata['deeplink'] = ()
-
     def _scraping(self, testing = False):
+        self.payloads = []
+        #self.contenidos_list = []
         req = self.sesion.get('https://www.hbo.com/documentaries/catalog')
         print(req.status_code, req.url)
-
+        #Sopa de la página principal con todos los documentales
         soup = BeautifulSoup(req.text, 'html.parser')
         contenedor = soup.find('div', {'class':'components/MovieGrid--container'})
         #print (contenedor)
-
+        #Dentro del contendor encontramos 448 contenidos (documentales)
         contenidos = contenedor.find_all('div', {'class':'modules/cards/CatalogCard--container modules/cards/DocumentaryCatalogCard--container modules/cards/CatalogCard--notIE modules/cards/CatalogCard--desktop'})
         print("cantidad de documentales:", len(contenidos))
-
-        deeplink_url_list = []
+        print(contenidos) 
+        return contenidos
+         
+                    
+    def get_name(self, contenidos):
+        title_list = []
         for documentaries in contenidos:
             title = documentaries.find('p', {'class':'modules/cards/CatalogCard--title'})
+            title_list.append(title.text)
+            print (title)
+        return title_list
+        print(title_list)
+    
+    def get_duration(self, contenidos):
+        duration_list = []          
+        for documentaries in contenidos:
             duration = documentaries.find('p', {'class':'modules/cards/CatalogCard--details'})
-            #print (title.text, duration.text)
-            
-            title_cleared = title.text.lower() #mayusculas por minusculas
-            title_cleared = title_cleared.replace(' ', '-') #espacios por guiones
-            title_cleared = title_cleared.replace('&','and') #& por and
-            title_cleared = title_cleared.replace(':','')#: por nada
-            title_cleared = title_cleared.replace('9/11','september-11')#9/11 por september-11
-            title_cleared = title_cleared.replace('/','-')#/ por -
-            title_cleared = title_cleared.replace('.','')#. por nada
-            title_cleared = title_cleared.replace('(','')#( por nada
-            title_cleared = title_cleared.replace(')','')#) por nada
-            title_cleared = title_cleared.replace(')','')#) por nada
-            title_cleared = title_cleared.replace('!','')#! por nada
-            title_cleared = title_cleared.replace('¢','cents')#¢ por centavos
-            title_cleared = title_cleared.replace("'",'') #' por centavos
-            title_cleared = title_cleared.replace("’",'') # ’ por nada
-            title_cleared = title_cleared.replace('12th-and-delaware', '12th-and-delaware-doc') #tiene un doc al final
-            title_cleared = title_cleared.replace('mavis', 'mavis-doc')
-            title_cleared = title_cleared.replace('quinceaneras', 'quinceanera')
-            #print(title_cleared)
-          
-            deeplink = 'https://www.hbo.com/documentaries/{}'.format(title_cleared)
-            deeplink_url_list.append(deeplink)
-       
-        #print(deeplink_url_list)
-        details_list = []
+            duration_list.append(duration.text)
+        return duration_list
+        print(duration_list)
 
-        try:
-            for clean_url in deeplink_url_list:
-                req_info = self.sesion.get(clean_url)
-                soup2 = BeautifulSoup(req_info, 'html.parser')
-                details = soup2.find('div', {'class':'modules/InfoSlice--assetDetails'})
-                details_list.append(details)
+    def documentaries_payload(self, contenidos, title_list, details_list):
+        title =  self.get_name(contenidos)
+        duration = self.get_duration(contenidos)
+        deeplink = self.get_deeplink(title_list)
+        cleanTitle = self.get_title_clean(title_list)
+        #yerar = self._get_year()
+        #rating = self_get_rating()      
+        
+        payload = { 
+            "PlatformCode": None, #self._platform_code, #Obligatorio 
+            "Id": None, #['_id'], #Obligatorio
+            "Title": None, #['name'], #Obligatorio 
+            "CleanTitle": None, #_replace(item['name']), #Obligatorio 
+            "OriginalTitle": None, #item['name'], 
+            "Type": None, #item['type'], #Obligatorio 
+            "Year": None, #None, #Important! 
+            "Duration": ['duration'],
+            "ExternalIds": None, #None,  #No estoy seguro de si es
+            "Deeplinks": { 
+            "Web": deeplink, #deeplink, #Obligatorio 
+            "Android":  None, 
+            "iOS": None, 
+            }, 
+            "Synopsis": None, #item['summary'], 
+            "Image": None, #[image],
+            "Rating": None, #item['rating'], #Important! 
+            "Provider": None, #None,
+            "Genres": None, #[item['genre']], #Important!
+            "Cast": None,
+            "Directors": None, #Important! 
+            "Availability": None, #Important! 
+            "Download": None, 
+            "IsOriginal": None, #Important! 
+            "IsAdult": None, #Important! 
+            "IsBranded": None, #Important! (ver link explicativo)
+            "Packages": 'Free', #Obligatorio 
+            "Packages": [{'Type':'free-vod'}],
+            "Country": None, 
+            "Timestamp": None, #datetime.now().isoformat(), #Obligatorio 
+            "CreatedAt": None, #self._created_at, #Obligatorio
+            }
+        self.payloads.append(payload)
+    print (documentaries_payload)
+        
+   
+    def get_title_clean(self, title):        
+        title_cleared = title.lower() #mayusculas por minusculas
+        title_cleared = title_cleared.replace(' ', '-') #espacios por guiones
+        title_cleared = title_cleared.replace('&','and') #& por and
+        title_cleared = title_cleared.replace(':','')#: por nada
+        title_cleared = title_cleared.replace('9/11','september-11')#9/11 por september-11
+        title_cleared = title_cleared.replace('/','-')#/ por -
+        title_cleared = title_cleared.replace('.','')#. por nada
+        title_cleared = title_cleared.replace('(','')#( por nada
+        title_cleared = title_cleared.replace(')','')#) por nada
+        title_cleared = title_cleared.replace(')','')#) por nada
+        title_cleared = title_cleared.replace('!','')#! por nada
+        title_cleared = title_cleared.replace('¢','cents')#¢ por centavos
+        title_cleared = title_cleared.replace("'",'') #' por centavos
+        title_cleared = title_cleared.replace("’",'') # ’ por nada
+        title_cleared = title_cleared.replace('12th-and-delaware', '12th-and-delaware-doc') #tiene un doc al final
+        title_cleared = title_cleared.replace('mavis', 'mavis-doc')
+        title_cleared = title_cleared.replace('quinceaneras', 'quinceanera')
+        return title_cleared
+        #print(title_cleared)
+     
+    def get_duration (self, documentaries):
+        duration = documentaries.find('p', {'class':'modules/cards/CatalogCard--details'})
+        return duration
+           
+    def get_deeplink(self, title_cleared):
+        deeplink = 'https://www.hbo.com/documentaries/{}'.format(title_cleared)
+        deeplink_url_list = []
+        deeplink_url_list.append(deeplink)
+       
+    def get_details(self, deeplink_url_list):
+        details_list = []
+        for details in deeplink_url_list:
+            try:
+                for clean_url in deeplink_url_list:
+                    req_info = self.sesion.get(clean_url)
+                    soup2 = BeautifulSoup(req_info, 'html.parser')
+                    details = soup2.find('div', {'class':'modules/InfoSlice--assetDetails'})
+                    details_list.append(details)
                 
-        except:
-            print("deeplink roto: " + deeplink )
+            except:
+                print("deeplink roto: ")
     
-        url_ejemplo = "https://www.hbo.com/documentaries/wig"
-        req_ejemplo = self.sesion.get(url_ejemplo)
-        print(req_ejemplo.status_code, req_ejemplo.url)
-        soup_ejemplo = BeautifulSoup(req_ejemplo.text, 'html.parser')
-        details_ejemplo = soup_ejemplo.find('div', {'modules/InfoSlice--assetDetails'})
-        print(details_ejemplo)
-    
-    payload = {
-        "Id": "1",
-        "Title": "Titanic",
-        "Type": "movie"# Lo pueden hacer completo.
-        }
-    
-    print(payload)
-    #self.mongo.insert("titanScraping", payload)
+    #url_ejemplo = "https://www.hbo.com/documentaries/wig"
+    #req_ejemplo = self.sesion.get(url_ejemplo)
+    #print(req_ejemplo.status_code, req_ejemplo.url)
+    #soup_ejemplo = BeautifulSoup(req_ejemplo.text, 'html.parser')
+    #details_ejemplo = soup_ejemplo.find('div', {'modules/InfoSlice--assetDetails'})
+    #print(details_ejemplo)
