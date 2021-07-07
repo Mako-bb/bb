e6e125cd76964da3d178bff8cf04f6fe7ca67ff8
matias <mi@bb.vision>
2021-07-07 12:44:33 -0300

    Update a hbo_mi, modularizando y documentando la clase.

--- platforms/hbo_mi.py
+++ platforms/hbo_mi.py
@@ -5,24 +5,23 @@ import datetime
 import requests
 import hashlib
 import platform
-import sys, os
+import sys
+import os
 import threading
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from common                 import config
-from bs4                    import BeautifulSoup, element
-from datetime               import datetime, timedelta
-from handle.mongo           import mongo
-from updates.upload         import Upload
-from handle.datamanager     import Datamanager
-from handle.replace         import _replace
-from selenium               import webdriver
+from common import config
+from bs4 import BeautifulSoup, element
+from datetime import datetime, timedelta
+from handle.mongo import mongo
+from updates.upload import Upload
+from handle.datamanager import Datamanager
+from handle.replace import _replace
+from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
 
 
 class HboMI():
@@ -42,44 +41,44 @@ class HboMI():
     para cargarlo y completar el resto de los datos.
     Al cargarse el deeplink de algunos contenidos no estan disponibles y ofrecen redirigir a HBOMAX.
     """
+
     def __init__(self, ott_site_uid, ott_site_country, type):
         self.ott_site_country = ott_site_country
-        self._config                = config()['ott_sites'][ott_site_uid]
-        self._created_at            = time.strftime("%Y-%m-%d")
-        self.mongo                  = mongo()
-        self._platform_code         = self._config['countries'][ott_site_country]   
-        self.titanPreScraping       = config()['mongo']['collections']['prescraping']
-        self.titanScraping          = config()['mongo']['collections']['scraping']
-        self.titanScrapingEpisodes  = config()['mongo']['collections']['episode']
-        self.sesion                 = requests.session()
-        self.skippedEpis            = 0
-        self.skippedTitles          = 0
+        self._config = config()['ott_sites'][ott_site_uid]
+        self._created_at = time.strftime("%Y-%m-%d")
+        self.mongo = mongo()
+        self._platform_code = self._config['countries'][ott_site_country]
+        self.titanPreScraping = config()['mongo']['collections']['prescraping']
+        self.titanScraping = config()['mongo']['collections']['scraping']
+        self.titanScrapingEpisodes = config(
+        )['mongo']['collections']['episode']
+        self.sesion = requests.session()
+        self.skippedEpis = 0
+        self.skippedTitles = 0
 
         if type == 'scraping':
             self._scraping()
-        
+
         elif type == 'testing':
             self._scraping(testing=True)
-        
+
         elif type == 'return':
             '''
             Retorna a la Ultima Fecha
             '''
 
-            params = {"PlatformCode" : self._platform_code}
+            params = {"PlatformCode": self._platform_code}
             lastItem = self.mongo.lastCretedAt(self.titanScraping, params)
             if lastItem.count() > 0:
                 for lastContent in lastItem:
                     self._created_at = lastContent['CreatedAt']
             self._scraping()
-    
 
-    def _scraping(self, testing = False):
+    def _scraping(self, testing=False):
         self.payloads = []
-
         self.episodes_payloads = []
         self.moviesPayloads()
-        #urls={'docums':'https://www.hbo.com/documentaries/catalog',
+        # urls={'docums':'https://www.hbo.com/documentaries/catalog',
         #        'movies':'https://www.hbo.com/movies/catalog',
         #        'series':'https://www.hbo.com/series/all-series'}
 
@@ -88,18 +87,22 @@ class HboMI():
         driver = webdriver.Chrome(PATH)
         req = self.sesion.get('https://www.hbo.com/movies/catalog')
         soup = BeautifulSoup(req.text, 'html.parser')
-        conteiner= soup.find('div', class_="components/MovieGrid--container")
-        contents=conteiner.find_all('div',{'class':'modules/cards/CatalogCard--container modules/cards/MovieCatalogCard--container modules/cards/CatalogCard--notIE modules/cards/CatalogCard--desktop'})
-        counter=0
+        conteiner = soup.find(
+            'div', {'class': 'components/MovieGrid--container'})
+        contents = conteiner.find_all('div', {
+                                      'class': 'modules/cards/CatalogCard--container modules/cards/MovieCatalogCard--container modules/cards/CatalogCard--notIE modules/cards/CatalogCard--desktop'})
+        counter = 0
         for content in contents:
-            image_list=[]
-            status=True
-            if counter==10:
+            self.image_list = []
+            self.details_dict = {}
+            status = True
+            if counter == 5:
                 return 1
-            title = content.find('p', {'class':'modules/cards/CatalogCard--title'}).text
-            title_depurate=self.depurateTitle(title)
+            title = content.find(
+                'p', {'class': 'modules/cards/CatalogCard--title'}).text
+            title_depurate = self.depurateTitle(title)
             deeplink = 'https://www.hbo.com/movies/{}'.format(title_depurate)
-            deeplinksDict={
+            deeplinksDict = {
                 "Web": deeplink,
                 'Android': None,
                 'iOS': None,
@@ -107,32 +110,25 @@ class HboMI():
             try:
                 driver.get(deeplink)
                 time.sleep(20)
-                html=driver.page_source
+                html = driver.page_source
                 soup_info = BeautifulSoup(html, 'html.parser')
             except:
-                status=False
+                status = False
             if status:
-                details = soup_info.find('div', {'class':'components/AiringDetailsBlock--airingDetailsBlock'})
-                childs = details.find_all('span', attrs={'class':'components/AiringDetailsBlock--detailsText'})
-                image_conteiner=soup_info.find('div', {'class':'components/HeroImage--heroImageContainer'})
-                image=image_conteiner.find('image')
-                imgage_url=image['xlink:href']
-                if '/content/dam' in imgage_url:
-                    imgage_url= 'https://www.hbo.com'+imgage_url
-                image_list.append(imgage_url)
-                self.get_details(childs)
-                type_='movie'
-                packages=self.get_packages()
-                #self.payloads.append(self.generic_payload(None,None,title,None,type_,year,duration,None,deeplinksDict,sinop,image_list,rating,genres,None,None,None,None,None,None,None))
+                self.get_details(soup_info)
+                self.get_image(soup_info)
+                packages = self.get_packages()
+                type_ = 'movie'
+                # self.payloads.append(self.generic_payload(None,None,title,None,type_,year,duration,None,deeplinksDict,sinop,image_list,rating,genres,None,None,None,None,None,None,None))
             else:
-                pass
-            counter+=1
+                print('No valid deeplink')
+            counter += 1
 
-    def seriesPayloads(self,contents):
+    def seriesPayloads(self, contents):
         #conteiner.find_all('div',{'class':'modules/cards/CatalogCard--container modules/cards/SamplingCatalogCard--container modules/cards/CatalogCard--notIE modules/cards/CatalogCard--desktop'})
         pass
 
-    def documsPayloads(self,contents):
+    def documsPayloads(self, contents):
         #conteiner.find_all('div',{'class':'modules/cards/CatalogCard--container modules/cards/DocumentaryCatalogCard--container modules/cards/CatalogCard--notIE modules/cards/CatalogCard--desktop'})
         pass
 
@@ -141,89 +137,89 @@ class HboMI():
         chars = ' *.,!/\|¬"£$%^_+{@<>:;¿?[]()}`='
         special1 = "'"
         special2 = '&'
-        newTitle = '' 
+        newTitle = ''
         if special1 in title:
-            title = title.replace(special1,'')
+            title = title.replace(special1, '')
         if special2 in title:
-            title = title.replace(special2,'and')
+            title = title.replace(special2, 'and')
         if '-' in title:
-            title = title.replace('-'," ")
+            title = title.replace('-', " ")
         for c in chars:
-            title = title.replace(c,'-')
+            title = title.replace(c, '-')
         for i in range(len(title)):
-            if title[i - 1] =='-' and title[i]=='-':
+            if title[i - 1] == '-' and title[i] == '-':
                 newTitle += ""
             else:
                 newTitle += title[i]
-        if newTitle[-1]=='-':
+        if newTitle[-1] == '-':
             newTitle = newTitle[:-1]
-        if newTitle[0]=='-':
+        if newTitle[0] == '-':
             newTitle = newTitle[1:]
         return newTitle
-    
-    def generic_payload(self,id_,crew,title,originalTitle,type_,year,duration,externalIds,deeplinks,
-            synopsis, image, rating, genres, cast, directors, availability, download, isoriginal, isadult, isbranded):
-        payload = { 
-            "PlatformCode": self._platform_code, #Obligatorio 
-            "Id": id_, #Obligatorio
+
+    def generic_payload(self, id_, crew, title, originalTitle, type_, year, duration, externalIds, deeplinks,
+                        synopsis, image, rating, genres, cast, directors, availability, download, isoriginal, isadult, isbranded):
+        payload = {
+            "PlatformCode": self._platform_code,  # Obligatorio
+            "Id": id_,  # Obligatorio
             "Crew": crew,
-            "Title": title, #Obligatorio 
-            "CleanTitle": _replace(title), #Obligatorio 
-            "OriginalTitle": originalTitle, 
-            "Type": type_, #Obligatorio #movie o serie 
-            "Year": year, #Important! 1870 a año actual 
-            "Duration": duration, 
+            "Title": title,  # Obligatorio
+            "CleanTitle": _replace(title),  # Obligatorio
+            "OriginalTitle": originalTitle,
+            "Type": type_,  # Obligatorio #movie o serie
+            "Year": year,  # Important! 1870 a año actual
+            "Duration": duration,
             "ExternalIds": externalIds,
-            "Deeplinks": deeplinks, 
-            "Synopsis": synopsis, 
-            "Image": image, 
-            "Rating": rating, #Important!  "Provider": , 
-            "Genres": genres, #Important! 
-            "Cast": cast, #Important! 
-            "Directors": directors, #Important! 
-            "Availability": availability, #Important! 
-            "Download": download, 
-            "IsOriginal": isoriginal, #Important! 
-            "IsAdult": isadult, #Important! 
-            "IsBranded": isbranded, #Important! (ver link explicativo)
-            "Packages": self.get_packages(), #Obligatorio 
-            "Country": [self.ott_site_country], 
-            "Timestamp": datetime.datetime.now().isoformat(), #Obligatorio 
-            "CreatedAt": self._created_at, #Obligatorio
+            "Deeplinks": deeplinks,
+            "Synopsis": synopsis,
+            "Image": image,
+            "Rating": rating,  # Important!  "Provider": ,
+            "Genres": genres,  # Important!
+            "Cast": cast,  # Important!
+            "Directors": directors,  # Important!
+            "Availability": availability,  # Important!
+            "Download": download,
+            "IsOriginal": isoriginal,  # Important!
+            "IsAdult": isadult,  # Important!
+            "IsBranded": isbranded,  # Important! (ver link explicativo)
+            "Packages": self.get_packages(),  # Obligatorio
+            "Country": [self.ott_site_country],
+            "Timestamp": datetime.datetime.now().isoformat(),  # Obligatorio
+            "CreatedAt": self._created_at,  # Obligatorio
         }
         return payload
 
-    def payload_episodes(self,id_,parentId,parentTitle,episode_num,season,crew,title,originalTitle,year,duration,externalIds,deeplinks,
-            synopsis, image, rating, provider, genres, cast, directors, availability, download, isoriginal, isadult, isbranded):
-        payload = { 
-            "PlatformCode": self._platform_code, #Obligatorio 
-            "Id": id_, #Obligatorio
-            "ParentId": parentId, #Obligatorio #Unicamente en Episodios
-            "ParentTitle":parentTitle , #Unicamente en Episodios 
-            "Episode": episode_num, #Obligatorio #Unicamente en Episodios 
-            "Season":season , #Obligatorio #Unicamente en Episodios
+    def payload_episodes(self, id_, parentId, parentTitle, episode_num, season, crew, title, originalTitle, year, duration, externalIds, deeplinks,
+                         synopsis, image, rating, provider, genres, cast, directors, availability, download, isoriginal, isadult, isbranded):
+        payload = {
+            "PlatformCode": self._platform_code,  # Obligatorio
+            "Id": id_,  # Obligatorio
+            "ParentId": parentId,  # Obligatorio #Unicamente en Episodios
+            "ParentTitle": parentTitle,  # Unicamente en Episodios
+            "Episode": episode_num,  # Obligatorio #Unicamente en Episodios
+            "Season": season,  # Obligatorio #Unicamente en Episodios
             "Crew": crew,
-            "Title":title , #Obligatorio 
-            "OriginalTitle": originalTitle, 
-            "Year": year, #Important! 
-            "Duration": duration, 
-            "ExternalIds":externalIds ,
+            "Title": title,  # Obligatorio
+            "OriginalTitle": originalTitle,
+            "Year": year,  # Important!
+            "Duration": duration,
+            "ExternalIds": externalIds,
             "Deeplinks": deeplinks,
-            "Synopsis": synopsis, 
-            "Image":image , 
-            "Rating":rating , #Important! 
-            "Provider": provider, 
-            "Genres": genres, #Important! 
-            "Cast": cast, #Important! 
-            "Directors": directors, #Important! 
-            "Availability": availability, #Important! 
-            "Download": download, 
-            "IsOriginal": isoriginal, #Important! 
-            "IsAdult": isadult, #Important! 
-            "IsBranded": isbranded, #Important! 
-            "Country": [self.ott_site_country], 
-            "Timestamp": datetime.datetime.now().isoformat(), #Obligatorio 
-            "CreatedAt": self._created_at, #Obligatorio
+            "Synopsis": synopsis,
+            "Image": image,
+            "Rating": rating,  # Important!
+            "Provider": provider,
+            "Genres": genres,  # Important!
+            "Cast": cast,  # Important!
+            "Directors": directors,  # Important!
+            "Availability": availability,  # Important!
+            "Download": download,
+            "IsOriginal": isoriginal,  # Important!
+            "IsAdult": isadult,  # Important!
+            "IsBranded": isbranded,  # Important!
+            "Country": [self.ott_site_country],
+            "Timestamp": datetime.datetime.now().isoformat(),  # Obligatorio
+            "CreatedAt": self._created_at,  # Obligatorio
         }
         return payload
 
@@ -231,12 +227,90 @@ class HboMI():
         '''
             Se hardcodea el package hasta averiguar como conseguirlo apropiadamente.
         '''
-        return [{'Type':'subscription-vod'}]
+        return [{'Type': 'subscription-vod'}]
 
-    def get_details(self, content_details):
-        #genres=''
-        #rating=''
-        #duration=''
-        #year=''        
-        #sinop = soup_info.find('div', {'class':'modules/Text--text modules/Text--headerHeavy components/RichText--richText'})
+    def get_details(self, content):
+        '''
+            Los campos de genres, rating, duration, year y sinopsis vienen juntos en un contenedor,
+            este metodo valida que el contenedor exista, ya que en algunos casos no viene (contenidos de hbomax).
+            Luego saca los childs y limpia los separadores. Finalmente ejecuta un metodo que va a revisar a que campo corresponde
+            cada child y lo asigna.
+        '''
+        try:
+            details = content.find(
+                'div', {'class': 'components/AiringDetailsBlock--airingDetailsBlock'})
+            childs = details.find_all(
+                'span', attrs={'class': 'components/AiringDetailsBlock--detailsText'}) 
+            for child in childs:
+                clean_child = child.text
+                if '|' in clean_child:
+                    clean_child = clean_child.split('|')[0]
+                    clean_child = clean_child.lower().strip()
+                    self.validate_key(clean_child)
+        except:
+            self.details_dict['genres'] = None
+            self.details_dict['rating'] = None
+            self.details_dict['duration'] = None
+            self.details_dict['year'] = None
+            self.details_dict['sinopsis'] = None
+        # return details
+    
+    def get_image(self, content):
+        '''
+            Algunas url vienen completas y otras no, por eso la validacion con el if
+            y se le concatena la parte faltante.
+        '''
+        try:
+            image_conteiner = content.find(
+                'div', {'class': 'components/HeroImage--heroImageContainer'})
+            image = image_conteiner.find('image')
+            imgage_url = image['xlink:href']
+            if '/content/dam' in imgage_url:
+                imgage_url = 'https://www.hbo.com'+imgage_url
+            self.image_list.append(imgage_url)
+        except:
+            self.image_list = None
+
+
+    def get_genres(self,content):
+        '''
+            Limpia los caracteres especiales de mas que pueda tener el genero como se recibe el dato.
+            Primero lo pasa a minusculas, luego valida si el genero pertenece a sci-fi, ya que
+            en este caso no corresponde eliminar el caracter "-", finalmente devuelve una lista.
+        '''
+        genres=[]
+        genre=content['genre'].lower()
+        chars='&/-_|,'
+        ok=True
+        for c in chars:
+            if c in genre:
+                ok=False
+                if (c=='-') and ('sci'in genre):
+                    pass
+                else:
+                    genres+=genre.split(c) 
+        if ok:
+            genres.append(genre)   
+        return genres
+    
+    def validate_key(self, value):
+        if 'hr' or 'min' in value:
+            duration = self.get_duration(value)
+            self.details_dict['duration'] = duration
+        elif value.isnumeric() and len(value) == 4:
+            self.details_dict['year'] = int(value)
+        elif (len(value) > 2) and ('.' or '-' not in value):
+            genres = self.get_genres(value)
+            self.details_dict['genres'] = genres
+        elif '.' not in value and value != 'hd':
+            self.details_dict['rating'] = value
+        else:
+            pass
+
+    def get_duration(self,value):
+        #duration=0
+        '''
+        Este metodo va a limpiar el dato de duracion y lo va a pasar a minutos int.
+        '''
         pass
+        #return duration
