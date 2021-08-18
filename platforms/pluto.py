33742c7546137976fc82ddd2b600b2127d40e625
Federico Giolli <fgiolli003@gmail.com>
2021-08-18 12:41:20 -0300

    platforms/pluto.tv: Cree la plataforma Pluto basado en el Template

---
+++ platforms/pluto.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import requests # Si el script usa requests/api o requests/bs4
+import time
+from bs4                import BeautifulSoup # Si el script usa bs4
+from selenium           import webdriver # Si el script usa selenium
+from handle.datamanager import Datamanager # Opcional si el script usa Datamanager
+from common             import config
+from handle.mongo       import mongo
+from updates.upload     import Upload
+from handle.replace     import _replace
+
+class Pluto():
+
+    def __init__(self, ott_platforms, ott_site_country, ott_operation):
+        self.test = ott_operation in ("testing", "return") #
+        config_ = config()['ott_sites'][ott_platforms] # Obligatorio
+        self.country = ott_site_country # Opcional, puede ser útil dependiendo de la lógica del script.
+        self._created_at = time.strftime('%Y-%m-%d')
+        self._platform_code = config_['countries'][ott_site_country]
+        self.mongo = mongo()
+        self.sesion                 = requests.session() # Requerido si se va a usar Datamanager
+        self.titanPreScraping       = config()['mongo']['collections']['prescraping'] # Opcional
+        self.titanScraping          = config()['mongo']['collections']['scraping'] # Obligatorio. También lo usa Datamanager
+        self.titanScrapingEpisodios = config()['mongo']['collections']['episode'] # Obligatorio. También lo usa Datamanager
+        self.skippedTitles          = 0 # Requerido si se va a usar Datamanager
+        self.skippedEpis            = 0 # Requerido si se va a usar Datamanager
+        self._url                   = config_['url']
+
+        #### OBLIGATORIO si se usa Selenium para que pueda correr en los servers
+        try:
+            if platform.system() == 'Linux':
+                Display(visible=0, size=(1366, 768)).start()
+        except Exception:
+            pass
+        ####
+
+        """
+        La operación 'return' la usamos en caso que se nos corte el script a mitad de camino cuando
+        testeamos, sea por un error de conexión u otra cosa. Nos crea una lista de ids ya insertados en
+        nuestro Mongo local, la cual podemos usar para saltar los contenidos scrapeados y volver rápidamente
+        a donde había cortado el script.
+        """
+        if ott_operation == 'return':
+            return_params = {'PlatformCode' : self._platform_code}
+            last_item = self.mongo.lastCretedAt('titanPreScraping', return_params)
+            if last_item.count() > 0:
+                for last_content in last_item:
+                    self._created_at = last_content['CreatedAt']
+            self.prescraped_ids = [pay["Id"] for pay in Datamanager._getListDB(self, self.titanPreScraping)]
+
+            self.scraping()
+        else:
+            self.prescraped_ids = list()
+
+        if ott_operation in ('testing', 'scraping'):
+            self.scraping()
+
+
+    def getButtonsMenu():
+        url = ""
+
+    def scraping(self):
+        res = requests.get("https://pluto.tv/en/on-demand")
+        web = res.content
+        soup = BeautifulSoup(web, "lxml")
+        menu = soup.find_all("button", {"class": "iconButton-0-2-108"})
+        button_links = []
+        for buttons in menu:
+
+
+
