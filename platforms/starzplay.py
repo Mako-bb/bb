b1c97e6940e192efcdca18dd65f26bcdfade64c2
DESKTOP-LM2PDQK\facun <facundo.e.arias@gmail.com>
2021-08-18 16:27:32 -0300

    platforms/starzplay : metodo para recorrer todos los films con selenium

--- platforms/starzplay.py
+++ platforms/starzplay.py
@@ -62,8 +62,105 @@ class StarzPlay():
         if ott_operation in ('testing', 'scraping'):
             self.scraping()
 
-                       
+
+
+    def scroll_page(self, driver):
+        SCROLL_PAUSE_TIME = 0.5
+        last_height = driver.execute_script("return document.body.scrollHeight")
         
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(SCROLL_PAUSE_TIME)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height     
+
+
+    def get_urls_films(self, driver):
+        urls_films_temp = []
+        container_urls = driver.find_elements_by_class_name('content-link')
+        
+        for urls in container_urls:
+            urls_films_temp.append(urls.find_element_by_tag_name('a').get_attribute('href'))
+
+        return urls_films_temp    
+
+
+
+    def get_data(self, driver, urls_films):
+
+        #PAYLOAD DE LA SALADA
+        """
+        payload_peliculas = {
+                    "PlatformCode":  self._platform_code,
+                    "Title":         movie['properties']['cardData']['text']['title'],
+                    "CleanTitle":    _replace(movie['properties']['cardData']['text']['title']),
+                    "OriginalTitle": None,
+                    "Type":          "movie",
+                    "Year":          None,
+                    "Duration":      None,
+
+                    "Id":            str(movie['properties']['cardData']['meta']['nid']),
+                    "Deeplinks": {
+
+                        "Web":       deeplink.replace('/tve?',''),
+                        "Android":   None,
+                        "iOS":       None,
+                    },
+                    "Synopsis":      movie['properties']['cardData']['text']['description'],
+                    "Image":         [movie['properties']['cardData']['images']],
+                    "Rating":        None,  # Important!
+                    "Provider":      None,
+                    "Genres":        [movie['properties']['cardData']['meta']['genre']],  # Important!
+                    "Cast":          None,
+                    "Directors":     None,  # Important!
+                    "Availability":  None,  # Important!
+                    "Download":      None,
+                    "IsOriginal":    None,  # Important!
+                    "IsAdult":       None,  # Important!
+                    "IsBranded":     None,  # Important!
+                    # Obligatorio
+                    "Packages":      [{'Type': 'tv-everywhere'}],
+                    "Country":       None,
+                    "Timestamp":     datetime.now().isoformat(),  # Obligatorio
+                    "CreatedAt":     self._created_at,  # Obligatorio
+                }
+        """
+
+        for film in urls_films:
+            time.sleep(3)
+            driver.get(film)
+    
+    
+    
     def scraping(self):
-        print("estoy corriendo el Template StarzPlay")
-        print(f'web a scrapear {self.url}')
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--start-maximized')
+        options.add_argument('--disable-extensions')
+        
+        path = 'C:\chromedriver'
+        driver = webdriver.Chrome(path,chrome_options=options)
+        driver.get(self.url)
+       
+        time.sleep(2)
+
+
+        #Clickeo la seccion buscar del nav
+        header_browse_link = driver.find_element_by_class_name('header-browse-link')
+        header_browse_link.click()
+        header_nav_link = header_browse_link.find_element_by_class_name('header-nav-link')
+        header_nav_link.find_element_by_tag_name('a').click()
+        
+        time.sleep(2)
+        #Busco el link que contiene todos los films
+        categories = driver.find_element_by_class_name('categories-blocks-wrapper')
+        categories.find_element_by_class_name('starz-link').click()
+
+        
+        self.scroll_page(driver)
+        
+        urls_films = self.get_urls_films(driver)
+        
+        self.get_data(driver, urls_films)
