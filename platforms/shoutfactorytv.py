3bbe76a4b1389c278f7cda00714374587ad8a412
danlucagutierrezbb <dlg@bb.vision>
2021-12-20 18:45:24 -0300

    Se crea funcionalidad para la extracción de información en shoutfactorytv.py

--- platforms/shoutfactorytv.py
+++ platforms/shoutfactorytv.py
@@ -40,8 +40,7 @@ class Shoutfactorytv():
         self.payloads_shows         = []    #Inicia payloads shows vacio, luego de la primer ejecución obtenemos contenido
         self.payloads_episodes      = []    #Inicia payloads episodios vacio, luego de la primer ejecución obtenemos contenido
         self.deeplinkBase           = "https://www.shoutfactorytv.com"
-        self.url                    = self._config['url']      #URL YAML
-        #self._format_url            = self._config['format_url'] 
+        self.url                    = self._config['url']   #URL YAML 
         self.testing                = False
         self.sesion                 =  requests.session()
         self.headers                 = {"Accept": "application/json",
@@ -71,22 +70,49 @@ class Shoutfactorytv():
         self.list_db_episodes = Datamanager._getListDB(self, self.titanScrapingEpisodios)
         self.list_db_movies_shows = Datamanager._getListDB(self, self.titanScraping)
         
-        #Se pasa por parametro el contenido de la plataforma y la función se encarga de extraer la información
-        self.get_payload_movies(requests.get(self.url)) 
+        self.get_payload_movies(requests.get(self.url))     #Se obtiene la request de la pagina y se extrae la información necesaria
 
         '''
         self.get_payload_shows(show_data) 
         self.get_payload_episodes(episode_data)
         '''
 
-        self.sesion.close() #Se cierra la session
+        self.sesion.close()
         #Upload(self._platform_code, self._created_at, testing=self.testing) #Sube a Misato (OJO, NO LO USAMOS TODAVÍA)
 
+    #Se encargar de llenar extraer información para llenar los payloads de peliculas y series
     def get_payload_movies(self, response):
 
-        print("Status code: " + str(response.status_code))
         if response.status_code == 200:
+            print("Status code: " + str(response.status_code))
+
             soup = BS(response.text, 'lxml')    #Se trae todo el contenido de la plataforma y lo convierte en un objeto de BS
-            contenedor = soup.find_all("div",{"class" : "divRow"})  #Contenido completo 
-            print(contenedor)
-        
+            categories = soup.find_all("div", class_ = 'divRow')  #Se queda con los tags que contienen las catergorias en una lista
+            links_movies = categories[0].find_all("a")  #Obtiene una lista con todos los links de las peliculas
+            links_shows = categories[1].find_all("a")   #Obtiene una lista con todos los links de las series
+
+            deeplinks_movies = self.get_categories_movies(links_movies)
+            deeplinks_series = self.get_categories_shows(links_shows)
+
+            '''
+            for link in deeplinks_movies:
+
+            for link in deeplinks_series:
+            '''
+
+        else:
+            print("Status code: " + str(response.status_code))
+            
+    #Getter que extraen las categorias de las peliculas
+    def get_categories_movies(self, links_movies):
+         deeplinks = []
+         for item in links_movies:    
+             deeplinks.append(self.deeplinkBase + item['href'])   #Agrega los links de las categorías de las peliculas en una lista  
+         return deeplinks
+
+    #Getter que extraen las categorias de las series
+    def get_categories_shows(self, links_series):
+         deeplinks = []
+         for item in links_series:    
+             deeplinks.append(self.deeplinkBase + item['href'])   #Agrega los links de las categorías de las series en una lista  
+         return deeplinks
